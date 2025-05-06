eb42a3fd6e9a58b154b704c8115be7ae90bb13d6
Najma-1437 <najmaw7341@gmail.com>
2025-05-06 22:56:41 +0300

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,3 +1,7 @@
 def is_leap(year):
     leap=False
-    if(year % 4== 0 and year % 100 != 0 ) or
+    if(year % 4== 0 and year % 100 != 0 ) or (year % 400 == 0):
+        leap=True
+    return leap
+year = int(input("Enter a year"))
+print(is_leap(year))
